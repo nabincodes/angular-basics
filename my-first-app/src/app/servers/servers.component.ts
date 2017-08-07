c24e40ae6cb36b6f537e0b9fcff10ef74b80fa68
nabincodes <nabinjaiswal@gmail.com>
2017-08-08 02:35:46 +0530

    added the data binding, directives and inputs

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No Server is created.';
+  serverName='';
+  serverCreated = false;
+  servers = ['Test Server', 'Test Server 2'];
+
+  constructor() {
+    setTimeout(()=>{
+      return this.allowNewServer = true;
+    },2000)
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = this.serverName + ' server was created';
+  }
+  updateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
