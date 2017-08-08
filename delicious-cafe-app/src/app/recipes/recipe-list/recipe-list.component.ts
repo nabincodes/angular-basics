254d4d740186942a5278911009766c8ee4656df7
nabincodes <nabinjaiswal@gmail.com>
2017-08-09 01:14:28 +0530

    coded the layout of recipe components and added a recipe model

--- delicious-cafe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ delicious-cafe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Pizza Recipe Description','https://cdn.pixabay.com/photo/2016/04/21/22/50/pizza-1344720_960_720.jpg'),
+    new Recipe('Pizza', 'Pizza Recipe Description','https://cdn.pixabay.com/photo/2016/04/21/22/50/pizza-1344720_960_720.jpg')
+  ];
 
   constructor() { }
 
