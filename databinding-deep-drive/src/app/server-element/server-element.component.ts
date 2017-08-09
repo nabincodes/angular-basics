e35c5f1b540f533d67c18e127e3c925deff36538
nabincodes <nabinjaiswal@gmail.com>
2017-08-10 02:31:29 +0530

    fully functional app with seperate components bind together

--- databinding-deep-drive/src/app/server-element/server-element.component.ts
+++ databinding-deep-drive/src/app/server-element/server-element.component.ts
@@ -1,13 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input() element: {type: string, name: string, content: string};
+  // since element object is only accessible in server-element component to make it globally accessible even in app-component we add @Input() directive. As a result we can bind element in app.component.html. Basically @Input() directive allows us to bind outside the component.
+  @Input('srvElement') element: {type: string, name: string, content: string};
+
 
   constructor() { }
 
