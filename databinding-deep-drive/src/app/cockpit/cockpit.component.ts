b70d0c7eb3135f8065a1e23603713395a71d2861
nabincodes <nabinjaiswal@gmail.com>
2017-08-11 02:12:35 +0530

    created the local instance instead on two-way binding in cockpit component

--- databinding-deep-drive/src/app/cockpit/cockpit.component.ts
+++ databinding-deep-drive/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,25 +9,26 @@ export class CockpitComponent implements OnInit {
   // We add @Output() decorator to serverCreated and blueprintCreated to make it listenable from outside
   @Output() serverCreated = new EventEmitter<{serverName:string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName:string, serverContent: string}>();
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput:HTMLInputElement) {
     this.serverCreated.emit({
-      serverName:this.newServerName,
-      serverContent:this.newServerContent
+      serverName:nameInput.value,
+      serverContent:this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput:HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName:this.newServerName,
-      serverContent:this.newServerContent
+      serverName:nameInput.value,
+      serverContent:this.serverContentInput.nativeElement.value
     });
   }
 }
