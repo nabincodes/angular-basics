dad2f5498857e53348da6e6fdb795f41f9696132
Nabin Jaiswal <nabinjaiswal@gmail.com>
2017-08-19 22:18:53 +0530

    added the account services

--- angular-services/src/app/account/account.component.ts
+++ angular-services/src/app/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import {LoggingService} from "../logging.service";
+import {LoggingService} from '../logging.service';
+import {AccountService} from '../account.service';
 
 @Component({
   selector: 'app-account',
@@ -10,12 +11,11 @@ import {LoggingService} from "../logging.service";
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService, private accountsService: AccountService) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    this.loggingService.logStatusChange(status);
+    this.accountsService.updateStatus(this.id, status);
+    // this.loggingService.logStatusChange(status);
   }
 }
